7ae6e1101043927c628ad372802f8005f0f89d6f
breezewy <15658150268@163.com>
2019-03-22 21:18:28 +0800

    first commit

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,105 +1,103 @@
 <template>
-  <div class="PostList">
-    <!-- 
-            在数据没有返回的时候，显示这个正在加载的gif
-    -->
-    <div class="loading" v-if="isLoading">
-      <img src="../assets/isLoading.gif">
+    
+    <div class="PostList">
+        <!-- 数据未返回的时候，显示这个加载动画 -->
+        <div class="loading"  v-if="isLoading">
+            <img src="../assets/isLoading.gif" alt="正在加载...">
+        </div>
+    
+
+        <!-- 这个是帖子列表 -->
+        <div class="posts" v-else>
+            <ul>
+                <li>
+                    <div class="toobar">
+                        <span @click="change" :class="[tab === ''?'active':'deactive']">全部</span>
+                        <span @click="change" :class="[tab === 'good'?'active':'deactive']">精华</span>
+                        <span @click="change" :class="[tab === 'share'?'active':'deactive']">分享</span>
+                        <span @click="change" :class="[tab === 'ask'?'active':'deactive']">问答</span>
+                        <span @click="change" :class="[tab === 'job'?'active':'deactive']">招聘</span>
+                    </div>
+                </li>
+                <li v-for="post in posts" :key="post.id">
+                    <!-- 头像 -->
+                    <!-- 这里的img是绑定的动态的src,所以一定要用v-bind,这里用语法糖: -->
+                    <img :src="post.author.avatar_url" alt="头像">
+
+                    <span class="allcount">
+                        <!-- 回复量 -->
+                        <span class="reply_count">{{post.reply_count}}</span>/
+                        <!-- 浏览量 -->
+                        <span class="visit_count">{{post.visit_count}}</span>
+                    </span>
+
+                    <!-- 帖子的分类 -->
+                    <span :class="[{put_good:(post.good == true),put_top:(post.top == true),
+                          'topiclist-tab':(post.good != true&& post.top != true)}]">
+                        <span>
+                            {{post | tabFormatter}}
+                        </span>
+                    </span>
+
+                    <!-- 标题 -->
+                    <router-link :to="{
+                        name:'post_content',
+                        params:{
+                            id:post.id,
+                            loginname:post.author.loginname
+                        }
+                    }">
+                         <span class="post_title">{{post.title}}</span>
+                    </router-link>
+                   
+
+
+                    <!-- 最后回复时间 -->
+                    <span class="last_reply">{{post.last_reply_at | formatDate}}</span>
+                </li>
+                <li>
+                  <pagination @handle = "renderlist"></pagination>
+                </li>
+            </ul>
+        </div>
+
+   
     </div>
-
-    <!-- 
-            数据一旦返回，就显示下边这个div,这个div就是主题帖子列表
-    -->
-    <div class="posts" v-else>
-      <ul>
-        <li>
-          <div class="toobar">
-            <span @click="change" :class="[tab === ''?'active':'deactive']">全部</span>
-            <span @click="change" :class="[tab === 'good'?'active':'deactive']">精华</span>
-            <span @click="change" :class="[tab === 'share'?'active':'deactive']">分享</span>
-            <span @click="change" :class="[tab === 'ask'?'active':'deactive']">问答</span>
-            <span @click="change" :class="[tab === 'job'?'active':'deactive']">招聘</span>
-          </div>
-        </li>
-        <li v-for="post in posts" :key="post.id">
-          <!-- 头像 -->
-          <img :src="post.author.avatar_url">
-          <!--动态绑定数据，一定爱用v-bind-->
-          <!-- 回复/浏览 -->
-          <span class="allcount">
-            <span class="reply_count">{{post.reply_count}}</span>/
-            <span class="visit_count">{{post.visit_count}}</span>
-          </span>
-          
-          <!-- 帖子的分类 -->
-          <span :class="[{put_good:(post.good == true),put_top:(post.top == true),'topiclist_tab':(post.top != true&& post.good != true)}]">
-            <span>{{post | tabFormat}}</span>
-          </span>
-
-          <!-- 标题 -->
-          <router-link :to="{
-              name:'post_content',
-              params:{
-                  id:post.id,
-                  name:post.author.loginname
-              }
-          }">
-            <span class="post_title">{{post.title}}</span>
-          </router-link>
-
-          <!-- 最终回复时间 -->
-          <span class="last_reply">
-            <!-- 管道符后边，加过滤器的名字，来确定时间 -->
-            {{post.last_reply_at | formatDate}}
-          </span>
-        </li>
-
-
-        <li>
-          <!-- 分页 -->
-          <Pagination @handleList="renderList"></Pagination>
-        </li>
-      </ul>
-    </div>
-  </div>
 </template>
 
 <script>
-import Pagination from './Pagination'
+import pagination from './Pagination'
 export default {
-  name: "PostList",
-  data() {
-    return {
-      // 默认没有加载
-      isLoading: false,
-      posts: [] ,//页面列表数组
-      postpage:1,
-      tab:''
-    };
-  },
-  components:{
-    Pagination
-  },
-  methods: {
-    getData() {
-      this.$http
-        .get("https://cnodejs.org/api/v1/topics", {
-          params:{
-            page: this.postpage,
-            limit: 20,
-            tab:this.tab
-          }
-        })
-        .then(res => {
-          this.isLoading = false; //加载成功后，去除动画
-          this.posts = res.data.data;
-        })
-        .catch(err => {
-          // 处理返回失败后的问题
-          console.log(err);
-        });
+    name:"PostList",
+    data(){
+        return {
+            isLoading:false,
+            posts:[],   //代表页面的列表数组
+            postPage:1, //传递给接口的参数 页码
+            tab:''
+        }
     },
-     change(event){
+    components:{
+      pagination
+    },
+    methods:{
+        getData(){
+            this.$http.get('https://cnodejs.org/api/v1/topics',{
+                params:{
+                    page:this.postPage,
+                    limit:20,
+                    tab:this.tab
+                }
+            })
+            .then(res=>{
+                this.isLoading = false;  //数据加载成功后，加载动画取消
+                this.posts = res.data.data;  //拿到数据以后，把数据复制给posts
+            })
+            .catch(err=>{
+                console.log(err);
+            })
+        },
+        change(event){
           if(event.currentTarget.innerText ==='全部'){
             this.tab = ''
           }else if(event.currentTarget.innerText ==='精华'){
@@ -115,21 +113,23 @@ export default {
           }
           this.getData();
         },
-    renderList(value){
-      this.postpage = value
-      this.getData();
-    } 
-  },
-  beforeMount() {
-    // getData()方法，要在页面载入之前开始执行，用beforeMount 钩子函数
-    this.isLoading = true; //加载成功之前，显示加载动画
-    this.getData(); //在页面加载之前，获取数据
-  }
-};
+        renderlist(value){
+          this.postPage = value;  //当接受到子组件Pagination 传递过来的页码参数后
+          this.getData();   //再次调用函数
+        }
+    },
+    beforeMount(){
+        // 在页面加载成功之前，显示正在加载的动画
+        this.isLoading = true;
+        // 在页面加载之前，发请求获取数据
+        this.getData();
+    }
+}
 </script>
 
+
 <style scoped>
-.PostList{
+  .PostList{
     background-color: #e1e1e1;
   }
   .posts {
@@ -172,13 +172,13 @@ export default {
   }
 
   .allcount {
-    width: 70px;
+    width: 80px;
     display: inline-block;
     text-align: center;
     font-size: 12px;
   }
 
-  .posts .reply_count {
+  .reply_count {
     color: #9e78c0;
     font-size: 14px;
   }
@@ -195,7 +195,7 @@ export default {
     margin-right: 10px;
   }
 
-   .posts .topiclist_tab {
+  .topiclist-tab {
     background-color: #e5e5e5;
     color: #999;
     padding: 2px 4px;
@@ -220,6 +220,7 @@ export default {
   .toobar {
     height: 40px;
     background-color: #f5f5f5;
+    padding-left:8px;
   }
 
   .toobar .deactive {
@@ -247,6 +248,13 @@ export default {
     text-align: center;
     padding-top: 300px;
   }
+  .toobar .active{
+    color:#fff;
+    font-size: 15px;
+    background:#80bd01;
+    border-radius:3px;
+    padding:3px 4px;
+  }
   .post_title{
     max-width:70%;
     overflow: hidden;
@@ -254,13 +262,6 @@ export default {
     white-space:nowrap;
     display: inline-block;
     vertical-align: middle;
-  }
-  .toobar .active{
-    color:#fff;
-    font-size: 15px;
-    background:#80bd01;
-    border-radius:3px;
-    padding:3px 4px;
   }
 </style>
 
